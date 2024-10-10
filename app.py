138105fcf31afa781cfb8f3ce1c0d3bf05bd7e16
Matias Linares <matias.linares@comprandoengrupo.net>
2024-10-10 11:47:33 -0300

    Remove sentry

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 from flask import Flask
-import sentry_sdk
 
-sentry_sdk.init(
-    dsn="https://d48295d7afbd72525f78c7a3df3510da@sentry.cegdev.net/6",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-)
 
 app = Flask(__name__)
 
@@ -17,4 +10,5 @@ def index():
 
 @app.route("/error")
 def error():
+    1/0
     raise Exception("Testing sentry")
